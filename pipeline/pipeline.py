3e0c8cd46b968101fbf93663bd421a0bce7ee502
AICHA LARIBIA <261724044+Aicha-Laribia@users.noreply.github.com>
2026-03-01 15:38:20 +0000

    git commit -m "fix: update pipeline.py to handle new data format"

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -4,4 +4,5 @@ arguments =sys.argv
 df=pd.DataFrame({"day": [1, 2], "numb_passenger": [3, 4]})
 df["month"] = int(arguments[1])
 print(df.head())
-print("month:", int(arguments[1]))
+df.to_parquet(f"output_{arguments[1]}.parquet")
+print("month:", arguments[1])
